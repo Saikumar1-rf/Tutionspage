0a3247a597148be75a2df6667668f23973c72934
Saikumar1-rf <saikumar.rfchh@gmail.com>
2024-10-12 13:35:11 +0530

    saik

--- src/Components/Studentuser.js
+++ src/Components/Studentuser.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-  FaSearch,
   FaUserAlt,
   FaEnvelope,
   FaBell,
--- src/Components/Tutorpage.js
+++ src/Components/Tutorpage.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Homepage from './Homepage';
 
 const Tutorpage = () => {
   const [tutors, setTutors] = useState([
